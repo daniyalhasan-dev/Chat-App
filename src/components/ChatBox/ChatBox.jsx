03159f85602896e895d0e65416fcfec3df5a96c3
Daniyal Hasan <daniyalhasan223@gmail.com>
2025-11-11 02:23:35 +0500

    Real Time Chat App

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import './ChatBox.css'
+import assets from '../../assets/assets'
 
 const ChatBox = () => {
   return (
     <div className='chat-box'>
       <div className="chat-user">
-        
+        <img src={assets.profile_img} alt="" />
+        <p>Daniyal Hasan <img src={assets.green_dot} className='dot' alt="" /></p>
+        <img src={assets.help_icon} className='help' alt="" />
+      </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, porro.</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img src={assets.pic1} className='msg-img' alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, beatae.</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="chat-input">
+        <input type="text" placeholder='Send a Message' />
+        <input type="file" accept='image/png, image/jpeg' id="image" hidden/>
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
       </div>
     </div>
   )
